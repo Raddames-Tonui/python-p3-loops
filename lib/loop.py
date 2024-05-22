a2c7a5fa28d7343108898257e1f15d94af102d6d
Raddames <raddamestonui48@gmail.com>
2024-05-22 19:29:36 +0300

    done

---
+++ lib/loop.py
@@ -0,0 +1,8 @@
+name = 10
+# for name in range(10):
+#     print(f"name {name}")
+
+
+while name < 5:
+  print("Looping!")
+  name += 1
--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10;
+    while i > 0:
+        print(i)
+        i -= 1
+    if i == 0:
+        print("Happy New Year!")
+    return i 
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_int = [squared * squared for squared in int_list]
+    return squared_int
+
+y = square_integers([1, 2, 3, 4, 5])
+print(y)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 ==0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    return i
+
+fizzbuzz()
